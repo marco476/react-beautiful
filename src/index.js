d3c944749ebec6aaec5794cf88055f29ca759c94
Marco Cante <marco.cante.081@gmail.com>
2019-04-28 09:52:24 +0200

    right export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as Dialog } from './components/Dialog';
 export { default as Snack } from './components/Snack';
 export { default as Image } from './components/Image';
+export { default as NewslettersPopup } from './components/NewslettersPopup';
